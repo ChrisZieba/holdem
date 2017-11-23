b2e556e3e5ac129914f03bf259a8ac9fd8249042
Chris Zieba <zieba.chris@gmail.com>
2017-11-22 21:51:25 -0800

    Simulation code temaplte

---
+++ src/sim.cpp
@@ -0,0 +1,46 @@
+/**
+  holdem
+
+  Copyright (c) 2017 Chris Zieba http://chriszieba.com
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <random>
+
+#include "emscripten.h"
+#include "Sim.h"
+#include "Eval/SevenEval.h"
+
+
+
+
+int sim(uint8_t h0, uint8_t h1) {
+  return 0;
+}
+
+// This is the functon that's exported for use in javaScript
+int gen(uint8_t *heroCards, uint8_t *villianRange) {
+  return 0;
+}
+
+
+int main() {}
