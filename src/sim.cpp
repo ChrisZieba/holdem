fcc00cde2150d51de8ec98d019d78aedbe3fcc27
Chris Zieba <zieba.chris@gmail.com>
2017-11-24 02:48:33 -0800

    Simulation code

--- src/sim.cpp
+++ src/sim.cpp
@@ -1,38 +1,50 @@
-/**
-  holdem
-
-  Copyright (c) 2017 Chris Zieba http://chriszieba.com
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-*/
-
 #include <iostream>
 #include <algorithm>
 #include <random>
-
-#include "emscripten.h"
 #include "sim.h"
 #include "eval/SevenEval.h"
 
 
+std::random_device rd;
+
+std::mt19937 rng(rd());
+std::uniform_int_distribution<int> uni(0, 51);
+
+int sim(uint8_t h0, uint8_t h1, uint8_t v0, uint8_t v1, uint8_t b0, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, int boardSize) {
+
+  int dealCount = 5 - boardSize;
 
 
-int sim(uint8_t h0, uint8_t h1) {
-  return 0;
+  int completeBoard[5] = { b0,b1,b2,b3,b4 };
+  int deck[52];
+
+  for (int i = 0; i < 52; i+=1) {
+    if (h0 == i || h1 == i || v0 == i || v1 == i || b0 == i || b1 == i || b2 == i || b3 == i || b4 == i) deck[i] = 255;
+    else deck[i] = i;
+  }
+
+
+  int cardIdx;
+
+
+  for (int j = 0; j < dealCount; j+=1) {
+
+    do {
+      cardIdx = uni(rng);
+    } while (deck[cardIdx] == 255);
+
+
+    completeBoard[boardSize+j] = deck[cardIdx];
+
+    // Mark the random card as in use
+    deck[cardIdx] = 255; 
+  }
+
+  // Get a value for each players hand then compare and return the win, loss or tie
+  int heroValue = SevenEval::GetRank(completeBoard[0], completeBoard[1], completeBoard[2], completeBoard[3], completeBoard[4], h0, h1);
+  int villianValue = SevenEval::GetRank(completeBoard[0], completeBoard[1], completeBoard[2], completeBoard[3], completeBoard[4], v0, v1);
+
+  if (heroValue > villianValue) return 1;
+  else if (heroValue < villianValue) return -1;
+  else return 0;
 }
