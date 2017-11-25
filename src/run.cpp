a71218adff1ee7d9eb7d493822b99dcf396f492c
Chris Zieba <zieba.chris@gmail.com>
2017-11-24 20:55:44 -0800

    Run the simulations

--- src/run.cpp
+++ src/run.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
+
 #include "emscripten.h"
 #include "sim.h"
 #include "util.h"
 
 extern "C" {
 
-// This is what gets called from JS
-int run(uint8_t *heroCards, uint8_t *villianRange, int villianRangeCount, uint8_t *board, int boardSize) {
+  int run(uint8_t *heroCards, uint8_t *villianRange, int villianRangeCount, uint8_t *board, int boardSize, int simCount) {
+    int totalWin = 0, totalLoss = 0, totalTie = 0, totalSimulations = 0;
 
-    int w = 0;
+    for (int i = 0; i < villianRangeCount; i+=2) {
+      int comboWin = 0, comboLoss = 0, comboTie = 0, comboSimulations = 0;
 
-    for (int i = 0; i < villianRangeCount; i+=1) {
+      // Only simulate combos that do not include any of the community cards and/or the hero cards
+      if (contains(heroCards, villianRange[i], 2) || contains(heroCards, villianRange[i+1], 2) || contains(board, villianRange[i], boardSize) || contains(board, villianRange[i+1], boardSize)) continue;
+
       for (int k = 0; k < simCount; k+=1) {
         int out = sim(heroCards[0], heroCards[1], villianRange[i], villianRange[i+1], board[0], board[1], board[2], board[3], board[4], boardSize);
+        totalSimulations+=1;
+        comboSimulations+=1;
 
-        if (out > 0) w+=1;
+        if (out > 0) {
+          totalWin+=1;
+          comboWin+=1;
+        } else if (out < 0) {
+          totalLoss+=1;
+          comboLoss+=1;
+        } else {
+          totalTie+=1;
+          comboTie+=1;
+        }
       }
+
+      EM_ASM({
+        Module.cc([$0,$1,$2,$3,$4,$5,$6]);
+      }, 0, comboSimulations, comboWin, comboTie, comboLoss, villianRange[i], villianRange[i+1]);
 
     }
 
-    EM_ASM_({
-      Module.print($0);
-    }, 1, w);
+    EM_ASM({
+      Module.cc([$0 ,$1,$2,$3,$4]);
+    }, 1, totalSimulations, totalWin, totalTie, totalLoss);
 
     return 0;
   }
-
-  return 0;
-}
 }
