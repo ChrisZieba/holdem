9035339a7d6aee6ba77ab4438c11924e59af28d4
Chris Zieba <zieba.chris@gmail.com>
2017-11-26 01:59:26 -0800

    Add function comment for sim

--- src/sim.cpp
+++ src/sim.cpp
@@ -10,6 +10,21 @@ std::random_device rd;
 std::mt19937 rng(rd());
 std::uniform_int_distribution<int> uni(0, 51);
 
+/**
+    Returns an int value based on two given hand values.
+
+    @param h0 The hero players first card, must be between 0 and 51 inclusive.
+    @param h1 The hero players second card, must be between 0 and 51 inclusive.
+    @param v0 The opponent players first card, must be between 0 and 51 inclusive.
+    @param v1 The opponent players second card, must be between 0 and 51 inclusive.
+    @param b0 The first card of the flop, between 0-51 or 255 for not dealt yet.
+    @param b1 The second card of the flop, between 0-51 or 255 for not dealt yet.
+    @param b2 The third card of the flop, between 0-51 or 255 for not dealt yet.
+    @param b3 The turn card, between 0-51 or 255 for not dealt yet.
+    @param b4 The river card, between 0-51 or 255 for not dealt yet.
+    @param boardSize The number of community cards dealt so far (can be 3,4,5).
+    @return A bool indicating if array contains the value.
+*/
 int sim(uint8_t h0, uint8_t h1, uint8_t v0, uint8_t v1, uint8_t b0, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, int boardSize) {
 
   int dealCount = 5 - boardSize;
