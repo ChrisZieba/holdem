35e8b406d7823f39bca156184203c817bded829a
Chris Zieba <zieba.chris@gmail.com>
2017-11-25 22:53:31 -0800

    Some more sim tests

--- tests/sim_tests.cpp
+++ tests/sim_tests.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <assert.h>
+#include <tgmath.h>
 
 #include "../src/sim.h"
 #include "../src/eval/SevenEval.h"
 
-int test(uint8_t h0, uint8_t h1, uint8_t v0, uint8_t v1, uint8_t b0, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, int boardSize) {
+/**
+    Returns an int value based on two given hand values.
+
+    @param h0 The hero players first card, must be between 0 and 51 inclusive.
+    @param h1 The hero players second card, must be between 0 and 51 inclusive.
+    @param v0 The opponent players first card, must be between 0 and 51 inclusive.
+    @param v1 The opponent players second card, must be between 0 and 51 inclusive.
+    @param b0..b4 The community cards, between 0-51 or 255 for not dealt yet.
+    @param boardSize The number of community cards dealt so far (can be 3,4,5).
+    @param expected The expected percentage of wins
+    @param error An acceptabe range of error +=%
+    @return The total number of wins from the simulation.
+*/
+int test(uint8_t h0, uint8_t h1, uint8_t v0, uint8_t v1, uint8_t b0, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, int boardSize, double expected, double error) {
   int totalWin = 0, totalLoss = 0, totalTie = 0, totalSimulations = 0;
-  // AsKs vs 5s6s shoud be about 63/37 preflop
+
   for (int i = 0; i < 100000; i+=1) {
     int out = sim(h0, h1, v0, v1, b0, b1, b2, b3, b4, boardSize);
 
@@ -16,70 +31,22 @@ int test(uint8_t h0, uint8_t h1, uint8_t v0, uint8_t v1, uint8_t b0, uint8_t b1,
     totalSimulations+=1;
   }
 
-  printf("hero:[%d %d] vilian:[%d %d] board:[%d %d %d %d %d] wins:%d, sims:%d\n",h0, h1, v0, v1, b0, b1, b2, b3, b4, totalWin, totalSimulations);
+  
+  double winRate = (totalWin/(totalSimulations/1.0))*100;
 
-  return totalWin;
+  printf("hero:[%d %d] vilian:[%d %d] board:[%d %d %d %d %d] win rate:%f\n", h0, h1, v0, v1, b0, b1, b2, b3, b4, winRate);
+
+  if (fabs(winRate-expected) > error) return 1;
+
+  return 0;
 }
 
 
 int main() {
-  test(0, 4, 36, 32, 255, 255, 255, 255, 255, 0); // AsKs vs 5s6s should be about 63% preflop
-  test(0, 4, 36, 32, 37, 1, 22, 255, 255, 3); // flop is 5h Ah 9d, AKs should be about 80%
-  test(0, 4, 36, 32, 37, 1, 22, 39, 255, 4); // flop is 5h Ah 9d, 5c AKs should be about 95%
+  assert(test(0, 4, 36, 32, 255, 255, 255, 255, 255, 0, 63, 2) == 0); // AsKs vs 5s6s should be about 63% preflop
+  assert(test(0, 4, 36, 32, 255, 255, 255, 255, 255, 0, 73, 2) == 1); // AsKs vs 5s6s should be about 63% preflop
+  assert(test(0, 4, 36, 32, 37, 1, 22, 255, 255, 3, 80, 1) == 0); // flop is 5h Ah 9d, AKs should be about 80%
+  assert(test(0, 4, 36, 32, 37, 1, 22, 39, 255, 4, 4, 1) == 0); // flop is 5h Ah 9d, 5c AKs should be about 95%
 
   return 0;
 }
-/*
-0 = As
-1 = Ah
-2 = Ad
-3 = Ac
-4 = Ks
-5 = Kh
-6 = Kd
-7 = Kc
-8 Qs
-9 Qh
-10 Qd
-11 Qc
-12 Js
-13 Jh
-14 Jd
-15 Jc
-16 10s 
-17 10h
-18 10d
-19 10c
-20 9s
-21 9h
-22 9d
-23 9c
-24 8s
-25 8h
-26 8d
-27 8c
-28 7s
-29 7h
-30 7d
-31 7c
-32 6s
-33 6h
-34 6d
-35 6c
-36 5s
-37 5h
-38 5d
-39 5c
-40 4s
-41 4h
-42 4d
-43 4c
-44 3s
-45 3h
-46 3d
-47 3c
-48 2s
-49 2h
-50 2d
-51 2c
-*/
